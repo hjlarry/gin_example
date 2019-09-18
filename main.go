7605eb2b3feaff7501a9464533532174279d104d
hjlarry <hjlarry@163.com>
2019-09-18 11:18:43 +0800

    http hot restart server

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"./pkg/setting"
 	"./routers"
@@ -18,6 +23,25 @@ func main() {
 		WriteTimeout:   setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
+	// 通过信号实现热更新
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			println(os.Getpid())
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
 
-	s.ListenAndServe()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
