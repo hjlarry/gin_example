893e18ddde9ec00134f59aa2c6214038a8cb1298
hjlarry <hjlarry@163.com>
2019-09-25 09:34:58 +0800

    support upload image

--- routers/router.go
+++ routers/router.go
@@ -22,6 +22,7 @@ func InitRouter() *gin.Engine {
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 
 	r.GET("/auth", api.GetAuth)
+	r.POST("/upload", api.UploadImage)
 	apiv1 := r.Group("/api/v1")
 	apiv1.Use(jwt.JWT())
 	{
